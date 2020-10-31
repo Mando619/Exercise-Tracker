b145989e702e4f39588588c9b3b54d42cc164b0d
Mando Estrada <libryum33@hotmail.com>
2020-10-30 17:10:35 -0700

    changed model into a plain array

--- models/Workout.js
+++ models/Workout.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkOutSchema = new Schema({
-  day: Date.now(),
-  //exercises: Array
-  exercises : [{
-    typeOf: String,
-    name: String,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number,
-    duration: Number,
-  }]
+  day:  Date,
+  exercises: Array
+//   exercises : [{
+//     typeOf: String,
+//     name: String,
+//     duration: Number,
+//     weight: Number,
+//     reps: Number,
+//     sets: Number,
+//     duration: Number,
+ 
 });
 
 const Workout = mongoose.model("Workout", WorkOutSchema);
